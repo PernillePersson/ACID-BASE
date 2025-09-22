7c986453e5651b601893cb3894f5fe4db4ba47d3
Pernille Persson <78405936+PernillePersson@users.noreply.github.com>
2025-09-22 14:56:56 +0200

    Update Views.sql
    View til konti kun tilknyttet én bruger

--- Views.sql
+++ Views.sql
@@ -7,3 +7,16 @@ SELECT
 FROM Customer c
 JOIN AccountCustomer ac ON c.id = ac.customerId
 JOIN Account a ON ac.accountId = a.id;
+
+--View til konti kun tilknyttet én bruger
+CREATE OR REPLACE VIEW SingleUserAccounts AS
+SELECT 
+    a.id   AS AccountId,
+    a.Balance,
+    c.id   AS CustomerId,
+    c.Name AS CustomerName
+FROM Account a
+JOIN AccountCustomer ac ON a.id = ac.accountId
+JOIN Customer c ON ac.customerId = c.id
+GROUP BY a.id, a.Balance, c.id, c.Name
+HAVING COUNT(ac.customerId) = 1;
